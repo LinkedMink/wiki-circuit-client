b23d7ee7e6381566d3c6e9d75f28304579993172
Harlan Sang <harlansang@gmail.com>
2019-10-17 15:55:46 -0500

    temp

--- src/Components/Screens/VisualizationScreen.js
+++ src/Components/Screens/VisualizationScreen.js
@@ -112,28 +112,7 @@ class VisualizationScreen extends React.Component {
       <div>
         <h2>Article: {this.getReadableId(this.props.match.params.id)}</h2>
         <Form>
-          <Form.Group controlId="depth">
-            <Form.Label>
-              Depth
-            </Form.Label>
-            <Row>
-              <Col xs="12" sm="8" md="6">
-                <Form.Control type="range" 
-                              min="1"
-                              max="3"
-                              step="1"
-                              value={this.state.depth} 
-                              onChange={this.handleChange} 
-                              required />
-              </Col>
-            </Row>
-            <Row>
-              <Col xs="4" sm="3" md="2" className="text-left">1</Col>
-              <Col xs="4" sm="3" md="2" className="text-center"></Col>
-              <Col xs="4" sm="3" md="2" className="text-right">3</Col>
-            </Row>
-          </Form.Group>
-          <Form.Group controlId="segmentCount">
+          <Form.Group className="range-selector" controlId="segmentCount">
             <Form.Label>
               Segments
             </Form.Label>
